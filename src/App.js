eaba52284af72f58ba0d370a793cf25f09ec84ab
argo <argo@argos-macbook-pro.home>
2023-05-13 19:19:11 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import DataBox from './components/DataBox';
+import GraphBox from './components/GraphBox';
+import AlertBox from './components/AlertBox';
+import coldImg from './icons/cold.png';
+import hotImg from "./icons/hot.png";
+import tempImg from "./icons/temp.png";
+import sosImg from "./icons/sos.png"
+import { useState } from 'react';
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <DataBox title="Temperature" data="21" unit="°C" img={tempImg} imgAlt="thermometer"/>
+        <DataBox title="Min Temperature" data="17" unit="°C" img={coldImg} imgAlt="snowflake - low temperature icon" />
+        <DataBox title="Max Temperature" data="25" unit="°C" img={hotImg} imgAlt="fire - hot temperature icon"/>
+        <DataBox title="Workplace situation" data="OK" img={sosImg} imgAlt="SOS text icon"/>
+      </div>
+      <div className='Container'>
+        <GraphBox title="Temperature"/>
+        <AlertBox title="Alert history"/>
+      </div>
     </div>
   );
 }
